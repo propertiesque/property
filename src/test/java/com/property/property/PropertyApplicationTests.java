4ce412a55065a129c16869df03c93958f2623308
propertiesque <92678336+propertiesque@users.noreply.github.com>
2022-03-08 00:59:21 +0530

    first commit

---
+++ src/test/java/com/property/property/PropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.property.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
